f04cbb4f4e31b4ff85117534b56d4e62b6265ab8
JeffreyBoehme <jeffrey.boehme2@gmail.com>
2021-07-11 15:49:53 +1000

    Completed HW

---
+++ js.js
@@ -0,0 +1,27 @@
+var delay = 3500;
+
+$('.herobutton').on("click", function(){
+    $('.hideMe').slideToggle();
+  });
+
+
+
+function switchhero(){
+    setTimeout(function() {
+    $(".jbhero").css("display", "grid");
+    $(".heropane").css("animation", "fadein 3s forwards");
+
+    console.log("switched");
+}, delay);
+}
+
+function switchheroback(){
+    setTimeout(function() {
+        $(".heropane").css("animation", "fadein 3s forwards");
+    $(".jbhero").css("display", "none");
+    console.log("switched back");
+}, delay);
+}
+
+
+switchheroback();
